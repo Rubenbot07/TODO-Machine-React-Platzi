022a3b159bb8be22a4ee541b87d6518e332b9546
rubenbot77 <rubenbot77@hotmail.com>
2024-08-14 16:32:42 -0500

    fixing bug in TodoCounter when you completed the only existing Todo

--- src/TodoCounter/index.jsx
+++ src/TodoCounter/index.jsx
@@ -2,7 +2,7 @@ import './TodoCounter.css'
 export function TodoCounter ({ totalTodos, completedTodos }) {
   return (
     totalTodos === completedTodos
-      ? (totalTodos > 1)
+      ? (totalTodos > 0)
           ? <h1 className='counterTitle'>Congratulations, You've completed all tasks</h1>
           : <h1 className='counterTitle'>Welcome! Create your first Todo</h1>
       : <h1 className='counterTitle'>You have completed <span style={{ fontWeight: 'bolder' }}>{completedTodos}</span> of <span style={{ fontWeight: 'bolder' }}>{totalTodos}</span> TODOS</h1>
